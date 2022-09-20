5aef101883c04a254b707135008095335354e98a
vikash-rajpoot-14 <105754316+vikash-rajpoot-14@users.noreply.github.com>
2022-09-20 21:44:27 +0530

    done 1

--- src/components/Info.js
+++ src/components/Info.js
@@ -15,7 +15,7 @@ function Info(props) {
     useEffect(() => {
         const getUsers = async () => {
             setLoading(true);
-            const url = `https://heliverse-assignment.herokuapp.com/users`
+            const url = `http://localhost:5000/users`
             let parsedData = await fetch(url);
             let users = await parsedData.json()
             setUsers(users);
