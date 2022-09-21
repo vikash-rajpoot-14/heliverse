6a166aad1e23809578e45195af1e68abfb0f78d4
vikash-rajpoot-14 <105754316+vikash-rajpoot-14@users.noreply.github.com>
2022-09-22 03:12:48 +0530

    lol

--- src/components/pagination.js
+++ src/components/pagination.js
@@ -1,16 +1,12 @@
-import React, { useState} from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Pagination = ({ usersPerPage, totalUsers, paginate }) => {
-const [pageN, setPageN] = useState(1)
 
     const pageNumbers = [];
 
     for (let i = 1; i <= Math.ceil(totalUsers / usersPerPage); i++) {
         pageNumbers.push(i);
-    }
-    const handleOnClick=(number)=>{
-         setPageN(number)
     }
     return (
         <nav>
@@ -18,7 +14,7 @@ const [pageN, setPageN] = useState(1)
             "8px" }}>
                 {pageNumbers.map((number, index) => {
                     return (<li key={index} className='page-item '>
-                        <Link onClick={() => { paginate(number);handleOnClick(number) }} to="/" style={{ "width": "1.5rem" }} className='page-link '>
+                        <Link onClick={() => { paginate(number)}} to="/" style={{ "width": "1.5rem" }} className='page-link '>
                             {number}
                         </Link>
                     </li>
