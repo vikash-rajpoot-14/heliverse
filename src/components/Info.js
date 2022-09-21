71762928a3bf558ce10c881bdd64a69274c7ec2f
vikash-rajpoot-14 <105754316+vikash-rajpoot-14@users.noreply.github.com>
2022-09-21 18:31:56 +0530

    filteration

--- src/components/Info.js
+++ src/components/Info.js
@@ -8,16 +8,18 @@ function Info(props) {
     const [usersPerPage] = useState(20);
     const [users, setUsers] = useState([]);
     const [searchTerm, setSearchTerm] = useState("")
-
+    const [Gender, setGender] = useState("select-option")
+    const [Domain, setDomain] = useState("select-option")
+    const [Avail, setAvail] = useState("select-option")
     // eslint-disable-next-line
     const [loading, setLoading] = useState(false);
 
     useEffect(() => {
         const getUsers = async () => {
             setLoading(true);
-            const devEnv = process.env.NODE_ENV !=="production";
-            const {REACT_APP_DEV_URL,REACT_APP_PROD_URL} = process.env;
-            const url = ` ${devEnv? REACT_APP_DEV_URL:REACT_APP_PROD_URL}`
+            const devEnv = process.env.NODE_ENV !== "production";
+            const { REACT_APP_DEV_URL, REACT_APP_PROD_URL } = process.env;
+            const url = ` ${devEnv ? REACT_APP_DEV_URL : REACT_APP_PROD_URL}`
             let parsedData = await fetch(url);
             let users = await parsedData.json()
             setUsers(users);
@@ -39,23 +41,81 @@ function Info(props) {
         <>
             <div className="container">
                 <h1>User Details</h1>
-                <form className="d-flex">
-                    <input className="form-control me-2"  onChange={(event)=>{setSearchTerm(event.target.value)}} type="search" placeholder="Search" aria-label="Search" />
-                    <button className="btn btn-outline-success" onClick={(event)=>{setSearchTerm(event.target.value)}} type="submit">Search</button>
-                </form>
+                <p>search:{searchTerm} gender:{Gender} domain:{Domain} Avail:-{Avail}</p>
+                <div className="selection d-flex flex-row" >
+                    <div className="select " style={{ "padding": "1.5rem" }}>
+                    <h4>Search</h4>
+                        <form className="form d-">
+                            <input className="form-control me-2" style={{ "width": "12rem" }} onChange={(event) => { setSearchTerm(event.target.value) }} type="search" placeholder="Search" aria-label="Search" />
+                        </form>
+                    </div>
+                    <div className="select " style={{ "padding": "1rem" }}>
+                        <h4>domain</h4>
+                        <select className="domain"  onChange={(e)=>{setDomain(e.target.value)}} style={{ "width": "12rem", "padding": "0.5rem", "margin": "0.5rem" }} aria-label="Default select example">
+                            <option defaultChecked>select-option</option>
+                            <option value="Sales">Sales</option>
+                            <option value="Finance">Finance</option>
+                            <option value="Marketing">Marketing</option>
+                            <option value="IT">IT</option>
+                            <option value="Management">Management</option>
+                            <option value="UI Designing">UI Designing</option>
+                            <option value="Business Development">Business Development</option>
+                        </select>
+                    </div>
+                    <div className="select" style={{ "padding": "1rem" }}>
+                        <h4>gender</h4>
+                        <select className="gender" onChange={(e)=>{setGender(e.target.value)}} style={{ "width": "12rem", "padding": "0.5rem", "margin": "0.5rem" }} aria-label="Default select example">
+                            <option defaultChecked>select-option</option>
+                            <option value="Male">Male</option>
+                            <option value="Female">Female</option>
+                            <option value="Agender">Agender</option>
+                            <option value="Bigender">Bigender</option>
+                            <option value="Non-binary">Non-binary</option>
+                            <option value="Polygender">Polygender</option>
+                            <option value="Genderfluid">Genderfluid</option>
+                            <option value="Genderqueer">Genderqueer</option>
+                        </select>
+                    </div>
+                    <div className="select " style={{ "padding": "1rem" }}>
+                        <h4>availability</h4>
+                        <select className="availibility" onChange={(e)=>{setAvail(e.target.value)}} style={{ "width": "12rem", "padding": "0.5rem", "margin": "0.5rem" }} aria-label="Default select example">
+                            <option defaultChecked>select-option</option>
+                            <option value={true}>Available</option>
+                        </select>
+                    </div>
+                </div>
                 <div className="row">
-                    {currentUsers.filter((item)=>{
-                      if(searchTerm.toLowerCase()===""){
+                    {users.filter((item) => {
+                      if(Gender==="select-option"){
                         return item
-                      }else if(item.first_name.toLowerCase().includes(searchTerm.toLowerCase())){
+                      }else if(item.gender.includes(Gender)){
                         return item
                       }
+                    }).filter((item)=>{
+                        if(Domain==="select-option"){
+                            return item
+                        }else if(item.domain.includes(Domain)){
+                            return item
+                        }
+                    }).filter((item)=>{
+                        if(searchTerm===""){
+                            return item
+                        }else if(item.first_name.toLowerCase().includes(searchTerm.toLowerCase())){
+                            return item
+                        }
+                    }).filter((item)=>{
+                        if(Avail==="select-option"){
+                            return item
+                        }else if(item.available?item:!item){
+                            return item
+                        }
                     }).map((item) => {
                         return (<div key={item.id} className="container col-md-4 my-3">
                             <Card storeTeam={props.storeTeam} item={item} />
                         </div>
                         )
-                    })}
+                    })
+                    }
                 </div>
             </div>
             <div className="container">
